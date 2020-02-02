42f1795a334b46f230926f422df12ca95c482a73
draciletka <49039227+draciletka@users.noreply.github.com>
2020-02-02 19:54:53 +0100

    Update bolek.py

--- bolek.py
+++ bolek.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-url = 'https://www.divadlobolkapolivky.cz/program/' 
-fullpage = requests.get(url)
-soup = BeautifulSoup(fullpage.text, 'lxml')
-dates = soup.find_all('div', class_='events-date')
-plays = soup.find_all('h2', itemprop='name')
-actors = soup.find_all('div', class_='events-info')
+url = 'https://www.divadlobolkapolivky.cz/program/' # Current program page.
+fullpage = requests.get(url) # It downloads full page.
+soup = BeautifulSoup(fullpage.text, 'lxml') # Page without tags.
+dates = soup.find_all('div', class_='events-date') # It searches for dates.
+plays = soup.find_all('h2', itemprop='name') # It searches for plays.
+actors = soup.find_all('div', class_='events-info') # It searches for actors.
 print('V divadle Bolka Polívky hrají tato představení:\n')
-for i in range(0, len(plays)):
+for i in range(0, len(plays)): # Is executed as many times as there are plays.
     print(dates[i].text[4:-1])
     print(plays[i].text)
     print(actors[i].text + '\n')
